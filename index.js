84a6774a41f3d2a8deca163499309c446b54b29e
Erbolking <me@erickaky.com>
2019-07-21 19:11:53 +0200

    updated schemas

--- index.js
+++ index.js
@@ -1,47 +1,128 @@
 const { createServer } = require('http');
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { PubSub } = require('graphql-subscriptions');
 const { importSchema } = require('graphql-import');
+const { GraphQLScalarType, Kind } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = importSchema('./graphql/schema.graphql')
 
 // Provide resolver functions for your schema fields
-const POST_ADDED = 'POST_ADDED';
-const pubsub = new PubSub();
+
+// in-memory storage just for test
+const books = [{
+  id: 'book1',
+  title: 'Some book 1',
+  published: 1563726154117,
+  authorId: 'erbol',
+  price: '777',
+  numberOfPages: 12
+}, {
+  id: 'book2',
+  title: 'Some book 2',
+  published: 1563626155111,
+  authorId: 'erbol',
+  price: '413',
+  numberOfPages: 12
+}, {
+  id: 'book3',
+  title: 'Some book 3',
+  published: 1563722155314,
+  authorId: 'erbol',
+  price: '23',
+  numberOfPages: 12
+}, {
+  id: 'book4',
+  title: 'Some book 3',
+  published: 1563727155115,
+  authorId: 'erbol',
+  price: '413',
+  numberOfPages: 63
+}]
+
+const authors = [{
+  id: 'erbol',
+  name: 'Erbol'
+}]
+
+const BOOK_ADDED = 'BOOK_ADDED';
+const pubsub = new PubSub()
 const resolvers = {
   Query: {
-    test: () => 'Hello world!',
-    post: () => ({
-      comments: [],
-      id: 'test',
-      text: 'Some post',
-      tags: ['some tag']
-    })
+    books: (parent, args, context) => {
+      if (args.id) {
+        return books.filter(book => book.id === args.id)
+      }
+      return books
+    },
+
+    authors: (parent, args, context) => {
+      if (args.id) {
+        return authors.filter(author => author.id === args.id)
+      }
+      return authors
+    },
   },
 
+  Book: {
+    author: (book) => {
+      return authors.find(author => author.id === book.authorId);
+    }
+  },
+
+  Author: {
+    books: (author, args) => {
+      if (args.id) {
+        return books.filter(book => book.id === args.id && book.authorId === author.id);
+      }
+      return books.filter(book => book.authorId === author.id);
+    }
+  },
+
+
   Mutation: {
-    createPost: (root, args, context) => {
-      console.log('root', root)
-      console.log('args', args)
-      console.log('context', context)
-      pubsub.publish(POST_ADDED, { postAdded: args.post });
-      return args.post
+    createBook: (root, args, context) => {
+      const { book } = args;
+      const authorId = book.authorId;
+      const author = authors.find(author => author.id === authorId);
+
+      if (author) {
+        book.id = +new Date();
+        books.push(book);
+        pubsub.publish(BOOK_ADDED, { bookAdded: args.book });
+
+        return book;
+      }
+
+      throw new UserInputError('Author is not found', {
+        invalidArgs: Object.keys(args),
+      });
     }
   },
 
   Subscription: {
-    postAdded: {
-      subscribe: () => pubsub.asyncIterator(POST_ADDED),
+    bookAdded: {
+      subscribe: () => pubsub.asyncIterator(BOOK_ADDED),
     },
   },
 
-  Post: {
-    tags () {
-      return ['ew', 'w']
-    }
-  }
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue(value) {
+      return new Date(value); // value from the client
+    },
+    serialize(value) {
+      return value.getTime(); // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value) // ast value is always in string format
+      }
+      return null;
+    },
+  }),
 };
 
 const app = express();
